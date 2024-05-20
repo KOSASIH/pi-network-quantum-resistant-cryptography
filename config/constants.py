c9ee6e6ee31e2957a96cdfe6febccb27d4c963b5
KOSASIH <kosasihg88@gmail.com>
2024-05-20 17:19:21 +0700

    Create constants.py

---
+++ config/constants.py
@@ -0,0 +1,13 @@
+# Constants used throughout the project
+
+# Cryptographic parameters
+CRYPTO_SHA3_DIGEST_SIZE = 64  # SHA-3 digest size in bytes
+CRYPTO_SHA3_ROUNDS = 24  # Number of rounds for SHA-3
+
+# Network settings
+NETWORK_NODE_PORT = 8080  # Port for the Pi Network node
+NETWORK_MAX_PEERS = 100  # Maximum number of peers per node
+NETWORK_PEER_DISCOVERY_INTERVAL = 60  # Peer discovery interval in seconds
+
+# Other configuration options
+LOGGING_LEVEL = "INFO"  # Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
