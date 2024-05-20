7ac543e9f462f252a84c2f8cb760036e24df81d6
KOSASIH <kosasihg88@gmail.com>
2024-05-20 10:16:53 +0700

    Update test_key.py

--- tests/test_keys/test_key.py
+++ tests/test_keys/test_key.py
@@ -83,4 +83,267 @@ class TestKey(unittest.TestCase):
         key2 = Key()
         key3 = Key()
         key.inplace_add(key2)
-        self.assertNotEqual(
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_add(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_add(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_sub(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_sub(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_sub(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_sub(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_mul(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_mul(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_mul(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_mul(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_truediv(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_truediv(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_truediv(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_truediv(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_floordiv(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_floordiv(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_floordiv(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_floordiv(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_mod(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_mod(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_mod(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_mod(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_pow(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_pow(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_pow(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_pow(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_lshift(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_lshift(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()key2.inplace_lshift(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_lshift(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_rshift(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_rshift(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_rshift(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_rshift(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_and(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_and(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_and(key)self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_and(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_xor(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_xor(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_xor(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_xor(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_or(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_or(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_or(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_or(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_eq(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_eq(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_eq(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_eq(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_ne(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_ne(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_ne(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_ne(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_lt(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_lt(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_lt(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_lt(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_le(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_le(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_le(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_le(key2)
+        self.assertEqual(key, key5)
+
+    def test_inplace_gt(self):
+        key = Key()
+        key2 = Key()
+        key3 = Key()
+        key.inplace_gt(key2)
+        self.assertNotEqual(key, key2)
+        self.assertNotEqual(key, key3)
+
+        key4 = key2.copy()
+        key2.inplace_gt(key)
+        self.assertEqual(key4, key3)
+
+        key5 = key.copy()
+        key.inplace_gt(key2)
+        self.assertEqual(key, key5)
